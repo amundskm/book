55895dc707ea1b2f172c3649e8ffd14cf92b5fb1
amundskm <amundskm@gmail.com>
2019-04-24 15:23:58 -0500

    All required parts of assignment complete.

--- book.js
+++ book.js
@@ -1,5 +1,11 @@
 let myLibrary = [];
 
+
+// Show new book form
+function showBookForm(){
+  document.getElementById("newBookForm").style.display = "inline";
+  document.getElementById("addNewBookBtn").style.display = "none";
+}
 // Create a New Book
 function book(title, author, pages, read){
   this.title = title
@@ -22,19 +28,25 @@ function addBookToLibrary(){
   author = document.getElementById("author").value;
   pages = document.getElementById("pages").value;
   if(document.getElementById("read").checked){
-    read = "true"
+    read = "True"
   }else{
-    read = "false"
+    read = "False"
   }
 
   if ((title === '') && (author === '') && (pages === '')){
     alert("One of your inputs is blank")
-  } else{
-    let newBook = new book(title, author, pages, read)
-    // when the html form for an new book is submitted, add the book to mylibary,
-    myLibrary.push(newBook)
-    // hide new book form
-    updateLibrary()
+  } else {
+    if(isNaN(pages)){
+      alert("Pages must be a number")
+    }else {
+      let newBook = new book(title, author, pages, read)
+      // when the html form for an new book is submitted, add the book to mylibary,
+      myLibrary.push(newBook)
+      // hide new book form
+      updateLibrary()
+      document.getElementById("newBookForm").style.display = "none";
+      document.getElementById("addNewBookBtn").style.display = "inline";
+    }
   }
 }
 // Show Each Book in myLibary on webpage
@@ -43,13 +55,19 @@ function updateLibrary(){
 
   let  cardContainer = document.getElementById("newBookCards");
 
-// TODO: // temporary way to make sure the list is updated correctly. Fix later.
+// TODO: // temporary way to make sure the list is updated correctly.
+// I want to be able to only update a single element
   while (cardContainer.firstChild) {
       cardContainer.removeChild(cardContainer.firstChild);
   }
 
   // create card for each book in myLibrary
   for (index in myLibrary){
+    if (myLibrary[index].read === "False"){
+      readBtnText = "I Have Read This Book"
+    } else {
+      readBtnText = "I Have not Read This Book"
+    }
     var template = `
      <div class="card"}">
         <div class="card-body">
@@ -60,7 +78,8 @@ function updateLibrary(){
             <li class="list-group-item book-element-pages">${myLibrary[index].pages}</li>
             <li class="list-group-item book-element-read">${myLibrary[index].read}</li>
           </ul>
-          <button type="submit" class="btn btn-primary delete-book" id="deleteBookBtn-${index}">Delete Book</button>
+          <button type="submit" class="btn btn-primary" onclick="deleteBook(${index})" >Delete Book</button>
+          <button type="submit" class="btn btn-primary" id="btn-${index}" onclick="readBook(${index})">${readBtnText}</button>
         </div>
       </div>`
 
@@ -71,12 +90,30 @@ function updateLibrary(){
     }
 }
 
+// Remove book from library
 function deleteBook(index){
-  myLibrary.splice(index)
+  myLibrary.splice(index, 1)
   updateLibrary()
 }
-// Remove book from libra
 
 // Add Button to Change any Book Status to Read
+
+function readBook(index){
+  console.log(myLibrary[index].read)
+  if(myLibrary[index].read === "True"){
+    myLibrary[index].read = "False"
+    console.log(myLibrary)
+    readBtn = document.getElementById(`btn-${index}`)
+    readTxt = document.createTextNode("I Have Not Read This Book")
+    readBtn.appendChild(readTxt)
+  }else{
+    myLibrary[index].read = "True"
+    console.log(myLibrary)
+    readBtn = document.getElementById(`btn-${index}`)
+    readTxt = document.createTextNode("I Have Read This Book")
+    readBtn.appendChild(readTxt)
+  }
+  updateLibrary()
+}
 
 // Optional
