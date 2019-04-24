c27ae1af62baa44a2b84324dd39349958fe00c56
amundskm <amundskm@gmail.com>
2019-04-24 11:36:04 -0500

    create document template for adding books

--- book.js
+++ book.js
@@ -16,9 +16,35 @@ function book(title, author, pages, read){
 }
 
 // Add Book to myLibrary
+  // event listener on addBook button, show form.
+function addBookToLibrary(title, author, pages, read){
+  // when the html form for an new book is submitted, add the book to mylibary,
+  myLibary.push(book(title, author, pages, read))
+  // hide new book form
+}
+// Show Each Book in myLibary on webpage
 
-// Show Each Book in myLibary
+function updateLibrary(){
 
+  cardContainer = document.querySelector(#newBookCards)
+
+  for (book in myLibrary){
+    let template = '
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-title" id="book-title"></h5>
+          <ul class= "list-group">
+            <li class="list-group-item book-element-title">#{book.title}</li>
+            <li class="list-group-item book-element-author">#{book.author}</li>
+            <li class="list-group-item book-element-pages">#{book.pages}</li>
+            <li class="list-group-item book-element-read">#{book.read}</li>
+        </div>
+      </div>
+    ';
+    cardContainer.appendChild(template)
+  }
+
+}
 // Add Button to Remove any Book in myLibary
 
 // Add Button to Change any Book Status to Read
